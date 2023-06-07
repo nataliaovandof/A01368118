02d11f9244aeac4e41848caf80760cdf7de87f3a
nataliaovandof <131408235+nataliaovandof@users.noreply.github.com>
2023-06-06 18:45:13 -0600

    Update evidencia_final_uf6.py

--- evidencia_final_uf6.py
+++ evidencia_final_uf6.py
@@ -7,7 +7,7 @@ import pandas as pd
 #import matplotlib.pyplot as plt
 
 st.title ('Police Incident Reports from 2018 and 2020 in San Francisco')
-
+st.DataFrame(df)
 df = pd.read_csv("https://drive.google.com/file/d/1sUXMxryvguQp81yseLxYymTX5RxaH0e9/view?usp=drive_link/Police_Department_Incident_Reports__2018_to_Present.csv")
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
