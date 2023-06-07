3179035e7a34934d7ed9d27475a98e10b1266e6a
nataliaovandof <131408235+nataliaovandof@users.noreply.github.com>
2023-06-06 19:03:54 -0600

    Update evidencia_final_uf6.py

--- evidencia_final_uf6.py
+++ evidencia_final_uf6.py
@@ -12,12 +12,13 @@ df = pd.read_csv("Police_Department_Incident_Reports__2018_to_Present.csv")
 st.dataframe(df)
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
-mapa=pd.DataFrame()
-mapa['Date']=df['Incident Date']
-mapa['Day']=df['Incident Day of Week']
-mapa['Police District']=df['Police District']
-mapa['Neighborhood']=df['Analysis Neoghborhood']
-mapa['Incident Category']=df['Incident Category']
+mapa = pd.DataFrame()
+mapa['Date'] = df['Incident Date']
+mapa['Day'] = df['Incident Day of Week']
+mapa['Police District'] = df['Police District']
+mapa['Neighborhood'] = df['Analysis Neighborhood']
+mapa['Incident Category'] = df['Incident Category']
+mapa['Incident Subcategory'] = df['Incident Subcategory']
 mapa['Resolution'] = df['Resolution']
 mapa['lat'] = df['Latitude']
 mapa['lon'] = df['Longitude']
